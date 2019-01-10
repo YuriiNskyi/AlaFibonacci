e11b30225725aa0d1c4006e101aea0b53c425885
YuriyNskiy <ruanaruy@gmail.com>
2019-01-10 22:50:51 +0200

    Add project files.

---
+++ AlaFibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlaFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
